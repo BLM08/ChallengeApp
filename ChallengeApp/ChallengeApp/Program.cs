9f3ca54707aeae100500fcc284ccd1e645c6c55b
BLM08 <micmis58@wp.pl>
2023-03-07 13:03:22 +0100

    Zadanie dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Zadanie domowe dzień 4
+
+string name = "Michał";
+string sex = "Mężczyzna";           
+var age = "23";
+
+Console.WriteLine("Imię :" + name);
+Console.WriteLine("Płeć :" + sex);
+Console.WriteLine("Wiek :" + age + "lata");
+
+if (age < 50)
+{
+    if (name == "Michał" && age < 30)
+    {
+        Console.WriteLine("Mężczyzna poniżej 30 roku życia");
+    }
+    else if(sex == "Mężczyzna" || age < 30)
+    {
+        Console.WriteLine("Michał , lat 23");
+    }
+}
+else if(age < 30 && sex == "Mężczyzna")
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}    
