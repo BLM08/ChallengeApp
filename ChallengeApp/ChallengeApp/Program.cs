4162b3265ccdcbdde23a1a790fe3382cd5426356
BLM08 <micmis58@wp.pl>
2023-03-09 15:43:46 +0100

    Zadanie domowe - Dzień 5

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,17 +1,1 @@
-﻿Console.WriteLine("podaj liczbę: ");
-string number = Console.ReadLine();
-char[] letters = number.ToArray();
-
-for (int i = 0; i < 10; i++)
-{
-    var result = 0;
-    foreach (char letter in letters)
-    {
-        int charToNumber = letter - '0';
-        if (charToNumber == i)
-        {
-            result++; 
-        }
-    }
-    Console.WriteLine("W liczbie: " + number + "cyfra " + i + "występuje " + result + "razy.");
-}
+﻿
